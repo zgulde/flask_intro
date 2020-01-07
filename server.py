87894b90bcba92b2d48958902d09070f9eca298c
Zach Gulde <zachgulde@gmail.com>
2020-01-07 14:19:19 -0600

    Add form lesson demo code

--- server.py
+++ server.py
@@ -1,39 +1,56 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from time import strftime
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return "This is the home page!"
+    return render_template(
+        "index.html",
+        the_date=strftime("%B %d, %Y"),
+        languages=["Python", "Bash", "ClojureScript", "R"],
+    )
+
+@app.route("/greeting")
+def show_greeting_form():
+    return render_template("greeting.html")
+
+@app.route("/greeting-result", methods=["POST"])
+def show_greeting_result():
+    users_first_name = request.form["first_name"]
+    return render_template(
+        "greeting-result.html",
+        name=users_first_name
+    )
 
 
 @app.route("/which-side-of-the-force")
 def force():
     return "The dark side"
 
+
 from random import randint
 
-@app.route('/roll-dice/<int:ndice>')
+
+@app.route("/roll-dice/<int:ndice>")
 def rolldice(ndice):
     rolls = [randint(1, 6) for _ in range(ndice)]
-    output = '<h1>Here are your dice rolls:</h1>'
-    output += '<br>'
+    output = "<h1>Here are your dice rolls:</h1>"
+    output += "<br>"
     for roll in rolls:
-        output += str(roll) + '<br>'
+        output += str(roll) + "<br>"
     print(output)
     return output
 
-@app.route('/i-can-html')
+
+@app.route("/i-can-html")
 def htmlpage():
     rolls = [randint(1, 6) for _ in range(10)]
-    return render_template(
-        'my-html-page.html',
-        rolls=rolls
-    )
+    return render_template("my-html-page.html", rolls=rolls)
 
 
 @app.route("/hello/<name>")
 def hello(name):
-    print('Will we see this?')
+    print("Will we see this?")
     return "Hello, " + name + "!"
